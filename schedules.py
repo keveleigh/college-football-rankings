3a1568bff38310aa94e6aa25bcafdaa393e63cc3
Kurtis Eveleigh <kurtis.eveleigh@gmail.com>
2013-05-12 23:47:28 -0700

    Moved Excel formulas into Python.
    Allows for better manipulation and formatting of data.

--- schedules.py
+++ schedules.py
@@ -6,19 +6,22 @@ ESPN. It uses BeautifulSoup Alpha 4 and xlwt.
 
 In order to use this, you will need to download bs4, lxml, and xlwt.
 
-Dictionary format: {Team Name: [ESPN ID, FBS/FCS, Wins, Losses, [Opponent1, Outcome1, Opponent1W, Opponent1L], [Opponent2, Outcome2, Opponent2W, Opponent2L], ... , [OpponentN, OutcomeN, OpponentNW, OpponentNL]]}
-
+Team dictionary format: {Team Name: [ESPN ID, FBS/FCS, Wins, Losses, [Opponent1, Outcome1, Opponent1W, Opponent1L], [Opponent2, Outcome2, Opponent2W, Opponent2L], ... , [OpponentN, OutcomeN, OpponentNW, OpponentNL]]}
+ID dictionary format: {ESPN ID: Team Name}
+Ranks dictionary format: {Rank Stat: Team Name}
 """
 
 import urllib2
 import re
 import datetime
 import xlwt
+import operator
 from urlparse import urlparse
 from bs4 import BeautifulSoup as bs
 
-allSchools = {};
-allIDs = {};
+allSchools = {}
+allIDs = {}
+allRanks = {}
 
 def _format_schedule_url(year, idNum):
     """Format ESPN link to scrape individual records from."""
@@ -31,10 +34,6 @@ def scrape_links(school, espn_schedule):
     url = urllib2.urlopen(espn_schedule);
 ##    print url.geturl();
     soup = bs(url.read(), ['fast', 'lxml']);
-##    nameTemp = soup.title.string.encode('ascii').split(' - ');
-##    nameLen = len(nameTemp[0]) - 14;
-##    school_name = nameTemp[0][0:nameLen];
-##    print school_name;
 
     record = soup.find('div', attrs={'id':'showschedule'})
     record = record.find_all(text=re.compile('\d{1,2}-\d{1,2}'));
@@ -43,7 +42,7 @@ def scrape_links(school, espn_schedule):
     record = record[0].split('-');
     allSchools[school].append(record[0]);
     allSchools[school].append(record[1]);
-    
+
     opponents = soup.find_all("li", "team-name");
     i = 4;
     for opp in opponents:
@@ -63,11 +62,12 @@ def scrape_links(school, espn_schedule):
             allSchools[school][i].append(temp[6]);
             i+=1;
         elif temp[4] == 'Postponed':
-            del allSchools[school][i];            
+            del allSchools[school][i];
 
 def get_schools():
-    global allSchools;
-    global allIDs;
+    global allSchools
+    global allIDs
+    global allRanks
     url = urllib2.urlopen('http://espn.go.com/college-football/teams');
     print url.geturl();
     soup = bs(url.read(), ['fast', 'lxml']);
@@ -95,8 +95,6 @@ def main():
     for school in allSchools:
         print school;
         scrape_links(school, _format_schedule_url(year, allSchools[school][0]));
-##    for game in get_games(year, iterable=True):
-##        print game;
     j = 1;
     wb = xlwt.Workbook()
     items = allSchools.items()
@@ -114,7 +112,7 @@ def main():
     for key, value in items:
         if value[1] == 'FCS':
             wb.add_sheet(key);
-        
+
     for key, value in items:
         if value[1] == 'FBS':
             longestOpp = 0;
@@ -122,15 +120,12 @@ def main():
             ws.write(0, 0, xlwt.Formula('HYPERLINK("#' + "'FBS Main Page'" + '!A1";"' + key + '")'));
             ws.write(1, 0, value[1]);
             ws.write(2, 0, value[2] + '-' + value[3]);
-            ws.write(3, 0, xlwt.Formula(str(value[2]) + '/(' + str(value[2]) + '+' + str(value[3]) + ')'));
-            ws1.write(ws1Row, ws1Col, xlwt.Formula("'" + key + "'!A6"));
-            ws1.write(ws1Row, ws1Col+1, xlwt.Formula('HYPERLINK("#' + "'" + key + "'" + '!A1";"' + key + '")'));
-            #if ws1Row == 19:
-            #    ws1Row = 0;
-            #    ws1Col+=3;
-            #else:
-            ws1Row+=1;
+            teamRec = float(value[2]) / (int(value[2]) + int(value[3])) # Fix?
+            ws.write(3, 0, teamRec)
+
             i = 1;
+            oppWins = 0
+            oppLoss = 0
             for team in value[4:]:
                 teamName = team[0];
                 if len(team) > 1:
@@ -139,16 +134,20 @@ def main():
                     teamOutc = '';
                 if(teamName == 'TX A&M-Commerce'):
                     teamName = 'Texas A&M-Commerce'
-                    ws.write(i, 1, teamName);                    
+                    ws.write(i, 1, teamName);
                     ws.write(i, 2, teamOutc);
                     ws.write(i, 3, int(1));
                     ws.write(i, 4, int(9));
+                    oppWins = oppWins + 1
+                    oppLoss = oppLoss + 9
                 elif teamName == 'NW Oklahoma St':
                     teamName = 'Northwestern Oklahoma State'
                     ws.write(i, 1, teamName);
                     ws.write(i, 2, teamOutc);
                     ws.write(i, 3, int(4));
                     ws.write(i, 4, int(7));
+                    oppWins = oppWins + 4
+                    oppLoss = oppLoss + 7
                 else:
                     ws.write(i, 1, xlwt.Formula('HYPERLINK("#' + "'" + teamName + "'" + '!A1";"' + teamName + '")'));
                     ws.write(i, 2, teamOutc);
@@ -159,6 +158,8 @@ def main():
                             offset = 1;
                         ws.write(i, 3, int(allSchools[teamName][2])-offset);
                         ws.write(i, 4, int(allSchools[teamName][3])-(1-offset));
+                        oppWins = oppWins + int(allSchools[teamName][2])-offset
+                        oppLoss = oppLoss + int(allSchools[teamName][3])-(1-offset)
                 try: #For non-DII/III Schools
                     ws.write(i, 5, xlwt.Formula("'" + teamName + "'!A6"));
                 except:
@@ -166,12 +167,14 @@ def main():
                 if len(teamName) > longestOpp:
                     longestOpp = len(teamName);
                 i+=1;
-            ws.write(i+1, 3, xlwt.Formula('SUM(D2:D' + str(i) + ')'));
-            ws.write(i+1, 4, xlwt.Formula('SUM(E2:E' + str(i) + ')'));
-            ws.write(4, 0, xlwt.Formula('D' + str(i+2) + '/(D' + str(i+2) + '+E' + str(i+2) + ')'));
-            ws.write(5, 0, xlwt.Formula('A4*A5'));
+            oppRec = float(oppWins) / (oppWins + oppLoss)
+            ws.write(i+1, 3, oppWins);
+            ws.write(i+1, 4, oppLoss);
+            ws.write(4, 0, oppRec);
+            ws.write(5, 0, teamRec * oppRec);
             ws.col(0).width = len(key)*350;
             ws.col(1).width = longestOpp*300;
+            allRanks[key] = teamRec*oppRec
             j+=1;
 
     j+=1;
@@ -201,7 +204,17 @@ def main():
             ws.col(0).width = len(key)*350;
             ws.col(1).width = longestOpp*300;
             j+=1;
-            
+
+    sortedRanks = sorted(allRanks.items(), key=operator.itemgetter(1), reverse=True)
+    for rank in sortedRanks:
+        print rank
+        ws1.write(ws1Row, ws1Col, rank[1]);
+        ws1.write(ws1Row, ws1Col+1, xlwt.Formula('HYPERLINK("#' + "'" + rank[0] + "'" + '!A1";"' + rank[0] + '")'));
+        #if ws1Row == 19:
+        #    ws1Row = 0;
+        #    ws1Col+=3;
+        #else:
+        ws1Row+=1;
     wb.save('AllTeams.xls');
 
 if __name__ == '__main__':
